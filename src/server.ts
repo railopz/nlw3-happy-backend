c9aff224c52f04e0f10bc6b3a84d5dd83597f150
Rai L Chaves <railopz@gmail.com>
2020-10-14 16:07:08 -0300

    :checking: finalized backend

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import express from "express";
+import cors from "cors";
+import "express-async-errors";
+
 import path from "path";
 import "./databases/connection";
 
 import routes from "./routes";
+import errorHandler from "./errors/handler";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
 app.listen(3333, () => {
   console.log("Started server port 3333");
--- src/views/images_views.ts
+++ src/views/images_views.ts
@@ -3,7 +3,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: image.path,
+      url: `http://localhost:3333/${image.path}`,
     };
   },
 };
